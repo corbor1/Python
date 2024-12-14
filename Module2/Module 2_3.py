fdea47588a94077ce0ad1324608b07539124402b
Corbor <corbor@mail.ru>
2024-12-14 09:45:43 +0300

    last modify Module 2_3.py

--- Module2/Module 2_3.py
+++ Module2/Module 2_3.py
@@ -2,9 +2,11 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 count = 0
 total_my_list = []
 while len(my_list) > count:
-    if my_list[count] >= 0:
+    if my_list[count] >= 1:
         total_my_list.append(my_list[count])
-        count +=1
+        count += 1
+    elif my_list[count] == 0:
+        count += 1
     else:
         break
-print (total_my_list)
+print(total_my_list)
