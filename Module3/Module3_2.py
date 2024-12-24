e8f692e73c882d0c789e42e9330c2b3fadcc9b92
Corbor <corbor@mail.ru>
2024-12-24 18:14:34 +0300

    Module3_2 modifid!

--- Module3/Module3_2.py
+++ Module3/Module3_2.py
@@ -5,7 +5,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
+    else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 send_email('Это сообщение для проверки связи', 'corbor194@gmail.com')
 
