28e5c45f0bb4c39fa00134e5e585c4fff7714341
corbor1 <Corbor@mail.ru>
2025-01-14 22:30:10 +0300

    modyfid Module 5_2

--- Module5/class Module 5_3.py
+++ Module5/class Module 5_3.py
@@ -16,27 +16,27 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей:{self.number_of_floors}'
 
-    def __eq__(self, other):
+    def __eq__(self, other):#== ??????????????
         return self.number_of_floors == other
 
-    def __lt__(self, other):
+    def __lt__(self, other): #<
         return self.number_of_floors<other
 
-    def __le__(self, other):
+    def __le__(self, other):# <=
         return self.number_of_floors <= other
 
-    def __gt__(self, other):
-        return self.number_of_floors <= other
+    def __gt__(self, other):#(>),
+        return self.number_of_floors > other
 
-    def __ge__(self, other):
+    def __ge__(self, other):#>=
         return self.number_of_floors >= other
 
-    def __ne__(self, other):
+    def __ne__(self, other):#!=
         return self.number_of_floors != other
 
     def __add__(self, other):
         self.number_of_floors += other
-        return self.number_of_floors
+        return f'Название: {self.name}, кол-во этажей:{self.number_of_floors}'
 
     def __radd__(self, other):
         self.number_of_floors += 1
@@ -49,11 +49,11 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-print(h1)
-print(h2)
-print(h1 == h2) # __eq__
+print(h1)#Название: ЖК Эльбрус, кол-во этажей:10
+print(h2)#Название: ЖК Акация, кол-во этажей:20
+print(h1 == h2) # __eq__ False
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10 # __add__ Название: ЖК Эльбрус, кол-во этажей:20
 
 print(h1)
 print(h1 == h2)
